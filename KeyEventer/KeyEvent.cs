4644a5f670064bd083e7ebfe6474c7b55f5a36d2
kokkiemouse <kokkiemouse@gmail.com>
2020-03-23 20:35:31 +0900

    sex

---
+++ KeyEventer/KeyEvent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KeyEventer
+{
+    public class KeyEvent
+    {
+        public event EventHandler Push;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int GetAsyncKeyState(int vKey);
+        private bool run_flag = false;
+        private CancellationTokenSource tokenSource;
+        private void LoopyMethod(CancellationToken token)       //実際の処理
+         
+        {
+            while (true)
+            {
+                //処理
+                for (int i = 1; i <= 256; i++)
+                {
+                    int rtn = GetAsyncKeyState(i);
+                    if ((rtn & 1) != 0)
+                    {
+                        //押されたら
+                        Push(this, EventArgs.Empty);
+                    }
+                }
+                if (token.IsCancellationRequested)
+                {
+                    run_flag = false;
+                    break;   //終了処理
+                }
+            }
+
+        }
+        public void Start()
+        {
+            if (run_flag == true) return;
+            tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+
+            Task.Run(() => LoopyMethod(token));
+            run_flag = true;
+
+        }
+        public void Stop()
+        {
+            if (run_flag == false) return;
+            tokenSource.Cancel();
+        }
+
+    }
+}
---
+++ KeySounder/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeySounder
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private System.Media.SoundPlayer player = null;
+
+        //WAVEファイルを再生する
+        private void PlaySound()
+        {
+            //再生されているときは止める
+            //if (player != null)
+                //StopSound();
+
+            //読み込む
+            //非同期再生する
+            player.Play();
+
+            //次のようにすると、ループ再生される
+            //player.PlayLooping();
+
+            //次のようにすると、最後まで再生し終えるまで待機する
+            //player.PlaySync();
+        }
+        private void StopSound()
+        {
+            if (player != null)
+            {
+                player.Stop();
+                player.Dispose();
+                player = null;
+            }
+        }
+
+        KeyEventer.KeyEvent keyEvent;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            keyEvent.Start();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            keyEvent.Stop();
+
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            keyEvent = new KeyEventer.KeyEvent();
+            keyEvent.Push += Push_Event;
+            player = new System.Media.SoundPlayer(System.AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\') + "\\takumi.wav");
+
+
+        }
+        private void Push_Event(object sender,EventArgs e)
+        {
+            PlaySound();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            keyEvent.Stop();
+        }
+    }
+}
